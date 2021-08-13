67ec9d8c79070ab95c50f57eea7befb33269a8c4
JRosadoDiaz <jose.rosado853@gmaill.com>
2021-08-12 23:29:46 -0600

    Changed some values to allow the project to access my database Included some method calls to test database connection and crud operations Also modified read() to take in one parameter for testing purposes

--- main.py
+++ main.py
@@ -3,13 +3,20 @@ import pandas
 import pymongo
 import json
 
-client = pymongo.MongoClient("localhost", 27017)
-db = client.projectdb
-people = db.people
+from pymongo.read_preferences import Secondary
+
+client = pymongo.MongoClient('mongodb://localhost:27017')#("localhost", 27017)
+db = client.test # Name of database
+people = db.people # Name of collection
 
 def main():
-    print(import_people('dataCsv.csv'))
-    # print(db_count())
+    #print(import_people('dataCsv.csv'))
+    #read({"_id":"1"})
+    #print(create_person("12345","jose","rosado", 2000))
+    update({"_id":"12345"}, {"first_name":"jeffrey"})
+    
+    #delete({"_id":"12345"})
+    print(db_count())
 
 def import_people(csv_path):
     print('importing csv')
@@ -21,10 +28,10 @@ def import_people(csv_path):
 
 def create_person(this_id, first_name, last_name, hire_year):
     result = people.insert_one({'_id': this_id, 'first_name': first_name, 'last_name': last_name, 'hire_year': hire_year})
-    return result.inserted_ids
+    return result.inserted_id
 
-def read(first, second):
-    results = people.find(first, second)
+def read(query): #(first, second)
+    results = people.find(query) # (first,second)
     for item in results:
         print(item)
 
@@ -33,11 +40,11 @@ def db_count():
 
 def update(object_to_update, updated_info):
     people.update_one(object_to_update, {'$set': updated_info})
-    read(object_to_update, {})
+    read(object_to_update)#, {})
 
 def delete(object_to_delete):
     people.delete_one(object_to_delete)
-    read(object_to_delete, {})
+    read(object_to_delete)#, {})
 
 if __name__ == "__main__":
     main()
